37a04bfd884906afdfecace8619366b7d513894e
vsigno <v.signorelli@ucl.ac.uk>
2020-10-20 18:48:22 +0100

    First commit
    - first commit

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-# ARGH! Mateys
-Augmented Reality Grounded Heritage: Portholes for Environmental and Youth Stories
+# ARGH! Mateys :anchor:
 
-ARGH! Mateys invites people to walk and discover environmental and youth stories hidden in digital portholes located in the Royal Docks. Find the keywords engraved on portholes placed in sites chosen by local young people, and use them to unlock messages from imagined futures either via either sms or Augmented Reality.
+*Augmented Reality Grounded Heritage: Portholes for Environmental and Youth Stories*
+
+---
+:earth_africa: [https://ucl-casa-ce.github.io/ARGH](https://ucl-casa-ce.github.io/argh)
+---
+
+##Description:
+
+__ARGH! Mateys__ invites people to walk and discover environmental and youth stories hidden in digital portholes located in the Royal Docks. Find the keywords engraved on portholes placed in sites chosen by local young people, and use them to unlock messages from imagined futures either via either SMS or Web Augmented Reality.
+
+
+##TODO
+- add multimarker support
+
+##Notes
+
+- if the camera points straight at the marker the model is unstable  
+
+## Technologies and libraries used
+
+- [Aframe](https://aframe.io/)
+- [AR.JS](https://github.com/AR-js-org/AR.js/)
+
---
+++ resources/cloak.js
@@ -0,0 +1,59 @@
+//Source https://stackoverflow.com/questions/56192021/how-to-declare-a-mask-material-using-a-frame-js
+AFRAME.registerComponent('cloak', {
+    init: function() {
+      let geometry = new THREE.BoxGeometry( 0.85, 0.85, 0.85);
+      geometry.faces.splice(4, 2) // cut out the top faces 
+      let material = new THREE.MeshBasicMaterial({
+         colorWrite: false
+      })
+      let mesh = new THREE.Mesh(geometry, material)
+      mesh.scale.set(1.1, 1.1, 1.1)
+      this.el.object3D.add(mesh)
+      console.log('Done');
+    }
+});
+
+AFRAME.registerComponent('cloak-gltf', {
+init: function() {
+  // make sure the model is loaded first
+  this.el.addEventListener('model-loaded', e=>{
+    let mesh = this.el.getObject3D('mesh') // grab the mesh
+    if (mesh === undefined) return;        // return if no mesh :(
+    mesh.traverse(function(node) {         // traverse through and apply settings
+      if (node.isMesh && node.material) {  // make sure the element can be a cloak
+        node.material.colorWrite = false
+        node.material.needsUpdate = true;
+        console.log('Done');
+      }
+    });
+  })
+}})
+
+//to improve the stability on mobile  https://stackoverflow.com/questions/53380400/aframe-smoothing-position-and-rotation
+AFRAME.registerComponent("listener", {
+  init: function() {
+    this.target = document.querySelector('#target');
+    this.prevPosition = null;
+    this.prevRotation = null;
+  },
+ tick: function() {
+   if (this.el.object3D.visible) {
+     this.target.setAttribute('visible', 'true')
+     if(this.prevPosition) {
+       this.target.object3D.position.lerp(this.prevPosition, 0.1)
+       let rot = this.target.object3D.rotation.toVector3().lerp(this.prevRotation, 0.5)
+       this.target.object3D.rotation.setFromVector3(rot)
+     } else {
+       this.target.setAttribute('position', this.el.getAttribute('position'))
+       this.target.setAttribute('rotation', this.el.getAttribute('rotation'))
+     }
+     this.prevPosition = this.el.object3D.position
+     this.prevRotation = this.el.object3D.rotation
+
+    } else {
+     this.target.setAttribute('visible', 'false')
+      this.prevPosition = null;
+      this.prevRotation = null;
+    }
+ }
+})
